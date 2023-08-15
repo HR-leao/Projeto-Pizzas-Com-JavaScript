0c922cf460672a7d55dd34048d02c0ff8cdfa426
Hendrio <bigdatahi7@gmail.com>
2023-08-15 19:46:45 -0300

    Up

--- script.js
+++ script.js
@@ -109,11 +109,17 @@ function updataCart() {
     if(cart.length > 0) {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for (let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt; 
             let cartItem = c('.models .cart--item').cloneNode(true);
+           
             let pizzSizeName;
-
             switch(cart [i].size) {
                 case 0:
                     pizzaSizeName = 'P';
@@ -129,8 +135,32 @@ function updataCart() {
             let  pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
             cartItem.querySelector('img').src = pizzaItem.img
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click',()=>{
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                }else {
+                    cart.splice(i,1);
+                }
+                updataCart();
+            })
+
+
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click',()=>{
+                cart[i].qt++;
+                updataCart();
+            })
+            
+
+
             c('.cart').append(cartItem);
         }
+        desconto = subtotal * 0.1;
+        total =subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
     } else {
         c('aside').classList.remove('show');
     }
