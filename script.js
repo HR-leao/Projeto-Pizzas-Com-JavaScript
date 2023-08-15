4f685c922c2afe7868d77014709362002bf7a311
Hendrio <bigdatahi7@gmail.com>
2023-08-15 17:33:48 -0300

    Up

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ modalQt = 1
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
 
+//listagem das pizzas
 
 pizzaJson.map((item, index)=>{
     console.log(item)
@@ -44,4 +45,18 @@ pizzaJson.map((item, index)=>{
 
     c('.pizza-area').append(pizzaItem);
 });
+
+
+// Eventos do Modal
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click',closeModal)
+})
 
